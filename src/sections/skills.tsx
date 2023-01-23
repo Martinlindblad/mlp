92c87d408bf66a2538c5f94f71e3f933683f90fa
Martin <36680040+Martinlindblad@users.noreply.github.com>
2023-01-23 01:01:58 +0100

    s

--- src/sections/skills.tsx
+++ src/sections/skills.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from 'react';
 import DevCard from 'src/components/devcard';
-import AnimatedContainer from 'src/components/layouts/AnimatedContainer';
+import AnimatedContainer from 'src/components/layouts/animatedContainer';
 import usePageCardsQuery from 'src/hooks/usePageCardsQuery';
 
 const Skills = (): JSX.Element => {
