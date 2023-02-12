6b976f69a0640f215995dc6be35d7cf0fdb87604
Martin <36680040+Martinlindblad@users.noreply.github.com>
2023-02-12 13:43:43 +0100

    stroke-width

--- src/sections/Navigation/Toggle.tsx
+++ src/sections/Navigation/Toggle.tsx
@@ -20,7 +20,7 @@ export const MenuToggle = ({
         aria-hidden="true"
         fill="none"
         stroke="currentColor"
-        stroke-width="1.5"
+        strokeWidth="1.5"
         viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg"
       >
@@ -37,7 +37,7 @@ export const MenuToggle = ({
         aria-hidden="true"
         fill="none"
         stroke="currentColor"
-        stroke-width="1.5"
+        strokeWidth="1.5"
         viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg"
       >
