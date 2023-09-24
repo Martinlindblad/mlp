dd35cfc3898a97bc2d31a55b59f91f5543f07fd8
Martin <36680040+Martinlindblad@users.noreply.github.com>
2023-09-24 23:37:59 +0200

    Test on frameworks

--- src/pages/frameworks.tsx
+++ src/pages/frameworks.tsx
@@ -6,11 +6,17 @@ const Frameworks = (): JSX.Element => {
   return (
     <Layout className="bg-slate-100 justify-center align-center flex-col dark:bg-gray-900 min-h-screen relative">
       <div className="realative overflow-hidden ">
-        <AnimatedFadeInContainer type="FadeInBottom">
-          <h1 className="text-lg font-normal lg:text-xl px-8  text-center lg:w-2/3 mx-auto flex pt-32 lg:pt-10 lg:items-center lg:justify-center w-full lg:container min-h-screen">
-            Frameworks
-          </h1>
-        </AnimatedFadeInContainer>
+        <main className="lg:container pt-24 lg:pt-0 ">
+          <div className="w-full lg:py-24 flex-col">
+            <div className="w-full h-full flex justify-center items-center relative">
+              <AnimatedFadeInContainer type="FadeInBottom">
+                <h1 className="text-lg font-normal lg:text-xl px-8  text-center lg:w-2/3 mx-auto flex pt-32 lg:pt-10 lg:items-center lg:justify-center w-full lg:container min-h-screen">
+                  Frameworks
+                </h1>
+              </AnimatedFadeInContainer>
+            </div>
+          </div>
+        </main>
       </div>
     </Layout>
   );
