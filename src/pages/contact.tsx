a5b4e2321a6c09a736332b089c5da62a5a2e2989
Martin <36680040+Martinlindblad@users.noreply.github.com>
2023-09-25 00:17:43 +0200

    Update contact.tsx

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Layout from '../components/Layouts/Layout';
 import ContactForm from '../components/ContactForm';
+import MainPageShortcuts from '../sections/MainPage/MainPageShortcuts';
 
 const Contact = (): JSX.Element => {
   return (
     <Layout className="bg-slate-100 justify-center align-center flex-col dark:bg-gray-900 min-h-screen relative">
+      <MainPageShortcuts />
       <ContactForm />
     </Layout>
   );
