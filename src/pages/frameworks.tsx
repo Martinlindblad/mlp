b462a16961ce761acf4206b37c52fdb3c5ecb6e4
Martin <36680040+Martinlindblad@users.noreply.github.com>
2023-09-24 23:23:46 +0200

    Animation container parent error tracked

--- src/pages/frameworks.tsx
+++ src/pages/frameworks.tsx
@@ -2,23 +2,18 @@ import React from 'react';
 import AnimatedFadeInContainer from '../components/Layouts/AnimatedFadeInContainer';
 import Layout from '../components/Layouts/Layout';
 
-// import Cases from '../sections/Cases/Cases';
 import Hero from '../sections/Hero/Hero';
-// import MainPageShortcuts from '../sections/MainPage/MainPageShortcuts';
 
 const Frameworks = (): JSX.Element => {
   return (
     <Layout className="bg-slate-100 justify-center align-center flex-col dark:bg-gray-900 min-h-screen relative">
-      <Hero />
-      {/* <MainPageShortcuts /> */}
-      {/* <Cases /> */}
-      <Layout className="bg-slate-100 justify-center align-center flex-col dark:bg-gray-900 min-h-screen relative">
+      <div className="realative overflow-hidden ">
         <AnimatedFadeInContainer type="FadeInBottom">
           <h1 className="text-lg font-normal lg:text-xl px-8  text-center lg:w-2/3 mx-auto flex pt-32 lg:pt-10 lg:items-center lg:justify-center w-full lg:container min-h-screen">
             Frameworks
           </h1>
         </AnimatedFadeInContainer>
-      </Layout>
+      </div>
     </Layout>
   );
 };
