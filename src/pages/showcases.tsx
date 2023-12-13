e3bd0c7d8914da6f3025373a112314058218e4eb
Martinlindblad <36680040+Martinlindblad@users.noreply.github.com>
2023-12-13 14:20:48 +0100

    Updated UI & added lazy loading to images on showcases

--- src/pages/showcases.tsx
+++ src/pages/showcases.tsx
@@ -1,6 +1,5 @@
 import { motion } from 'framer-motion';
-
-import { useMemo, useReducer as useReducer } from 'react';
+import { useMemo, useReducer } from 'react';
 import useProjectsAndCasesQuery from '../hooks/useProjectsAndCasesQuery';
 import PageLoader from '../components/AnimatedComponents/ContentLoader';
 import Link from 'next/link';
@@ -64,7 +63,6 @@ const ShowCases = () => {
     };
 
     const transition = 0.5;
-
     return {
       hidden: {
         opacity: 1,
@@ -105,59 +103,54 @@ const ShowCases = () => {
       {isLoading || !cases ? (
         <PageLoader />
       ) : (
-        <div className="flex flex-col md:flex-row w-full flex-wrap">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           {cases.map((item) => (
             <AnimatedFadeInContainer
               type="FadeInBottom"
-              className="w-full md:w-2/4 lg:h-46 py-2 sm:p-4 overflow-hidden"
+              className="lg:h-64 py-2 sm:p-4 overflow-hidden relative"
               key={`${item._id}-Showcase-item`}
               onTouchStart={() => handleInteraction(item._id.toString())}
               onTouchEnd={() => handleInteraction(item._id.toString())}
               onMouseEnter={() => handleInteraction(item._id.toString())}
               onMouseLeave={() => handleInteraction(item._id.toString())}
             >
-              <div
-                className="bg-center bg-no-repeat bg-cover bg-gray-700 bg-blend-multiply relative overflow-hidden "
-                style={{ backgroundImage: `url('${item.imageSource}')` }}
+              <img
+                src={item.imageSource}
+                alt={item.title}
+                loading="lazy"
+                className="w-full h-full object-cover object-center absolute top-0 left-0 z-0"
+              />
+              <motion.div
+                animate={caseState[item._id.toString()] ? 'visible' : 'hidden'}
+                variants={exit}
+                className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-28 relative z-10"
               >
-                <motion.div
-                  animate={
-                    caseState[item._id.toString()] ? 'visible' : 'hidden'
-                  }
-                  variants={exit}
-                  className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-28"
-                >
-                  <h1 className="mb-4 font-extrabold tracking-tight leading-none text-white text-xl md:text-2xl lg:text-3xl text-center">
-                    <span className="inline-block mb-2 text-white">
-                      {item.title}
-                    </span>
+                <h1 className="mb-4 font-extrabold tracking-tight leading-none text-white text-xl md:text-2xl lg:text-3xl text-center">
+                  <span className="inline-block mb-2 text-white">
+                    {item.title}
+                  </span>
+                </h1>
+              </motion.div>
+              <motion.div
+                animate={caseState[item._id.toString()] ? 'visible' : 'hidden'}
+                variants={enter}
+                className="flex flex-col items-center justify-center w-full h-full absolute top-0 left-0 z-10"
+                style={{
+                  background: `linear-gradient(rgba(${item.from}, 0.5), rgba(${item.to},0.5))`,
+                }}
+              >
+                <div className="flex flex-col items-center">
+                  <h1 className="text-xl md:text-2xl lg:text-3xl text-center font-bold">
+                    {item.description}
                   </h1>
-                </motion.div>
-                <motion.div
-                  animate={
-                    caseState[item._id.toString()] ? 'visible' : 'hidden'
-                  }
-                  variants={enter}
-                  className="flex flex-col items-center justify-center w-full h-full absolute z-10 top-0 left-0 overflow-hidden"
-                  style={{
-                    background: `linear-gradient(rgba(${item.from}, 0.5), rgba(${item.to},0.5))`,
-                  }}
-                >
-                  <div className="flex flex-col items-center">
-                    <h1 className="text-xl md:text-2xl lg:text-3xl text-center font-bold">
-                      {item.description}
-                    </h1>
-                    <Link
-                      href={`/cases/${item._id}`}
-                      className="flex-grow sm:flex-grow-0 inline-flex cursor-pointer items-center justify-center mt-10 ring-offset-2 ring-2 rounded-lg px-4 py-2 outline outline-2 outline-offset-2 hover:animate-pulse"
-                    >
-                      <span className="inline-block text-white">
-                        Go to case
-                      </span>
-                    </Link>
-                  </div>
-                </motion.div>
-              </div>
+                  <Link
+                    href={`/cases/${item._id}`}
+                    className="flex-grow sm:flex-grow-0 inline-flex cursor-pointer items-center justify-center mt-10 ring-offset-2 ring-2 rounded-lg px-4 py-2 outline outline-2 outline-offset-2 hover:animate-pulse"
+                  >
+                    <span className="inline-block text-white">Go to case</span>
+                  </Link>
+                </div>
+              </motion.div>
             </AnimatedFadeInContainer>
           ))}
         </div>
